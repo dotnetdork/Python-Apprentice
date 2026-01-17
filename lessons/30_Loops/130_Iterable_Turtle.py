0e6f8be5843458b6597767e234add695a2335621
Jay Sausa <jaysausa@student.purdueglobal.edu>
2026-01-17 15:21:06 -0500

    Fixed 130_Iterable_Turtle.ipynb to correctly be a .py

---
+++ lessons/30_Loops/130_Iterable_Turtle.py
@@ -0,0 +1,24 @@
+"""
+Programmable turtle graphics
+
+Use what you've learned about lists, loop, cycle, slice and zip to draw a pattern
+"""
+
+t = ... # Create a turtle like in previous programs, like 04_Crazy_Tina.py
+
+colors = ... # Make a list of colors
+
+directions = [ # Create a list of directions and angles
+    ( <angle>, <distance> ),
+    ( <angle>, <distance> ), ... # Add more
+]
+
+# Zip the colors and directions together, then unpack them. THere is a good example of this
+# in 10_More_iterables.ipynb in the discussion of zip()
+
+for ... in zip( ... , ...):
+    t.color( ... )
+    t.forward( ... )
+    t.left( ... )
+
+# Don't forget the special way to end a turtle program. 
