dd0c4b6e16a2ed5d8a35935e2e85bb4080f48897
Jay Sausa <jaysausa@student.purdueglobal.edu>
2026-01-17 15:27:12 -0500

    Fixed the errors in Iterable Turtle

--- lessons/30_Loops/130_Iterable_Turtle.py
+++ lessons/30_Loops/130_Iterable_Turtle.py
@@ -9,11 +9,11 @@ t = ... # Create a turtle like in previous programs, like 04_Crazy_Tina.py
 colors = ... # Make a list of colors
 
 directions = [ # Create a list of directions and angles
-    ( <angle>, <distance> ),
-    ( <angle>, <distance> ), ... # Add more
+    ( ), # Add an (angle, distance) tuple for each line
+    ( ), 
 ]
 
-# Zip the colors and directions together, then unpack them. THere is a good example of this
+# Zip the colors and directions together, then unpack them. There is a good example of this
 # in 10_More_iterables.ipynb in the discussion of zip()
 
 for ... in zip( ... , ...):
